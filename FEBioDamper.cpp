fec69ce963160143aa1489b5ffc16e8de5b7390b
Vraj Parikh <vrajp12@gmail.com>
2020-05-16 12:41:56 -0700

    Initial commit.

---
+++ FEBioDamper.cpp
@@ -0,0 +1,48 @@
+#include <FECore/sdk.h>
+#include "FEIsotropicFourier.h"
+#include "FEPlotHeatFlux.h"
+#include "FEHeatFlux.h"
+#include "FEConvectiveHeatFlux.h"
+#include "FEHeatSolver.h"
+#include "FEHeatSolidDomain.h"
+#include "FEHeatDomainFactory.h"
+#include "FEHeatSource.h"
+#include "FEHeatSolidDomain.h"
+#include "FEGapHeatFlux.h"
+
+FECORE_PLUGIN void PluginInitialize(FECoreKernel& fecore)
+{
+	// Set the kernel to match FEBio's kernel
+	FECoreKernel::SetInstance(&fecore);
+
+	// Create the damper module
+	fecore.CreateModule("damper");
+
+	// Domain factory
+	fecore.RegisterDomain(new FEDamperDomainFactory);
+
+	// Feature classes that you want to add to the plugin.
+	// FETASK_ID : Create a task class
+	// FESOLVER_ID : Create a solver class
+	// FEMATERIAL_ID : Create a material class
+	// FEBODYLOAD_ID : Create a body load class
+	// FENLCONSTRAINT_ID : Create a nonlinear constraint class
+	// FECOORDSYSMAP_ID : Create a coordinate system class
+	// FEPLOTDATA_ID : Create a plot data class
+	// FECALLBACK_ID : Create a callback class
+	REGISTER_FECORE_CLASS(FEHeatSolidDomain		, FEDOMAIN_ID								 , "heat-solid");
+	REGISTER_FECORE_CLASS(FEHeatSolver        , FESOLVER_ID                , "heat");
+	REGISTER_FECORE_CLASS(FEIsotropicFourier  , FEMATERIAL_ID              , "isotropic Fourier");
+	REGISTER_FECORE_CLASS(FEConstHeatSource   , FEBODYLOAD_ID              , "heat_source");
+	REGISTER_FECORE_CLASS(FEHeatFlux          , FESURFACELOAD_ID           , "heatflux");
+	REGISTER_FECORE_CLASS(FEConvectiveHeatFlux, FESURFACELOAD_ID           , "convective_heatflux");
+	REGISTER_FECORE_CLASS(FEPlotHeatFlux      , FEPLOTDATA_ID              , "heat flux");
+	REGISTER_FECORE_CLASS(FEGapHeatFlux       , FESURFACEPAIRINTERACTION_ID, "gap heat flux");
+
+	fecore.SetActiveModule(0);
+}
+
+FECORE_PLUGIN int GetSDKVersion()
+{
+	return FE_SDK_VERSION;
+}
---
+++ FEDamperDomainFactory.cpp
@@ -0,0 +1,25 @@
+#include "FEDamperDomainFactory.h"
+#include "FEHeatTransferMaterial.h"
+#include "FEHeatSolidDomain.h"
+
+//-----------------------------------------------------------------------------
+//! Use the material and the element type to determine the domain type.
+FEDomain* FEDomainDomainFactory::CreateDomain(const FE_Element_Spec& spec, FEMesh* pm, FEMaterial* pmat)
+{
+	// TODO Add logic here...
+	FEModel& fem = *pmat->GetFEModel();
+	FE_Element_Class eclass = spec.eclass;
+	const char* sztype = 0;
+	if (dynamic_cast<FEHeatTransferMaterial*>(pmat))
+	{
+		if (eclass == FE_ELEM_SOLID) sztype = "heat-solid";
+	}
+
+	if (sztype)
+	{
+		FEDomain* pd = fecore_new<FEDomain>(FEDOMAIN_ID, sztype, &fem);
+		if (pd) pd->SetMaterial(pmat);
+		return pd;
+	}
+	else return 0;
+}
